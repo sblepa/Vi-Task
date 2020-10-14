7473138a1808954952330b41270fca7a5985b4f7
Sahar Lepa <slepa@augury.com>
2020-10-15 00:23:58 +0300

    Init the store

--- app/src/main/java/com/vi/vimarvel/store/MarvelStore.java
+++ app/src/main/java/com/vi/vimarvel/store/MarvelStore.java
@@ -1,4 +1,18 @@
 package com.vi.vimarvel.store;
 
+import android.content.Context;
+
 public class MarvelStore {
+
+    private MarvelStoreState state;
+
+    private static MarvelStore sharedInstance;
+
+    private MarvelStore() {
+        state = new MarvelStoreState();
+    }
+
+    public static void initSharedStore(Context context) {
+        sharedInstance = new MarvelStore();
+    }
 }
--- app/src/main/java/com/vi/vimarvel/store/MarvelStoreState.java
+++ app/src/main/java/com/vi/vimarvel/store/MarvelStoreState.java
@@ -1,4 +1,9 @@
 package com.vi.vimarvel.store;
 
+import com.vi.vimarvel.store.models.MarvelCharacterModel;
+
+import java.util.ArrayList;
+
 public class MarvelStoreState {
+    private ArrayList<MarvelCharacterModel> marvelCharacters;
 }
